ca3d9169e3a5e17ff66f5627eae0b5da8da6ee36
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-04-29 23:34:02 +0900

    Cria o `ConsultaSaldoComponent`.
    - Comando:
      ng generate component components/consulta-saldo
    - Chama o componente a partir do template do `AppComponent`.
    - Exibe os dados de um cliente hardcode.

---
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Cliente } from 'src/app/models/cliente';
+
+@Component({
+  selector: 'app-consulta-saldo',
+  templateUrl: './consulta-saldo.component.html',
+  styleUrls: ['./consulta-saldo.component.css'],
+})
+export class ConsultaSaldoComponent implements OnInit {
+
+  public cliente: Cliente = new Cliente(
+    'Maria das Couves',
+    99.99,
+    new Date(),
+  );
+
+  constructor(
+  ) {
+  }
+
+  public ngOnInit(): void {
+  }
+
+}
