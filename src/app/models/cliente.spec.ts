9497bdcabe3bbde65b7b03c50a79d56ddeaa3099
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-04-30 21:13:17 +0900

    Exibe crédito pré-aprovado, quando houver crédito.

--- src/app/models/cliente.spec.ts
+++ src/app/models/cliente.spec.ts
@@ -15,4 +15,18 @@ describe('Cliente', () => {
     expect(cliente).toBeTruthy();
   });
 
+  describe('creditoPreAprovado', () => {
+
+    it('não deve liberar crédito para clientes de baixo saldo', () => {
+      cliente.saldo = 9999.99;
+      expect(cliente.creditoPreAprovado).toBe(0);
+    });
+
+    it('deve liberar R$ 20.000 de crédito para clientes de alto saldo', () => {
+      cliente.saldo = 10000.00;
+      expect(cliente.creditoPreAprovado).toBe(20000);
+    });
+
+  });
+
 });
--- src/app/models/cliente.ts
+++ src/app/models/cliente.ts
@@ -1,10 +1,32 @@
 export class Cliente {
 
   constructor(
+    /**
+     * Nome completo do cliente (nome + sobrenome).
+     */
     public nome: string,
+
+    /**
+     * Saldo em reais do cliente.
+     */
     public saldo: number,
+
+    /**
+     * Data de último acesso (último login) do cliente.
+     */
     public ultimoAcesso: Date,
   ) {
+  }
+
+  /**
+   * Crédito pré-aprovado automaticamente para o cliente em função de seu saldo.
+   */
+  public get creditoPreAprovado(): number {
+    if (this.saldo >= 10000) {
+      return 20000;
+    } else {
+      return 0;
+    }
   }
 
 }
