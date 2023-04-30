be8a9222746708e5c71a4a2b9c0ab8f317a6272d
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2023-04-30 20:48:20 +0900

    Introduz método `classNamePorValor`.

--- src/app/components/consulta-saldo/consulta-saldo.component.spec.ts
+++ src/app/components/consulta-saldo/consulta-saldo.component.spec.ts
@@ -22,4 +22,37 @@ describe('ConsultaSaldoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('classNamePorValor', () => {
+
+    it('deve apontar classe "zero" para 0', () => {
+      expect(component.classNamePorValor(0)).toBe('zero');
+    });
+
+    it('deve apontar classe "negativo" para -0.005', () => {
+      expect(component.classNamePorValor(-0.005)).toBe('negativo');
+    });
+
+    it('deve apontar classe "zero" para -0.004', () => {
+      expect(component.classNamePorValor(-0.004)).toBe('zero');
+    });
+
+    it('deve apontar classe "negativo" para -1', () => {
+      expect(component.classNamePorValor(-1)).toBe('negativo');
+    });
+
+    it('deve apontar classe "zero" para 0.004', () => {
+      expect(component.classNamePorValor(0.004)).toBe('zero');
+    });
+
+    it('deve apontar classe "positivo" para 0.005', () => {
+      expect(component.classNamePorValor(0.005)).toBe('positivo');
+    });
+
+    it('deve apontar classe "positivo" para 1', () => {
+      expect(component.classNamePorValor(+1)).toBe('positivo');
+    });
+
+  });
+
 });
